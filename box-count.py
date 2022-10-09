15e215321b2e66a7ac01beb32f47ab93b1036ae5
walacevale <wallacevale123@gmail.com>
2022-10-09 19:31:21 -0300

    final commit

--- box-count.py
+++ box-count.py
@@ -1,48 +1,38 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import cv2
 import os
-#from scipy.signal import convolve2d
-from tqdm import tqdm
-from Func import *
+from Func import th, gray
+##____________________________________________________#
 
+imagem = os.path.join('Dados', '3.png')
+img_th = th(gray(cv2.imread(imagem)))
 
-# abrir imagem direto da pasta Dados
-img = os.path.join('Dados', '3.png')
-# transforma a imagem em escala de cinza e dps em preto e branco
-# utilizei a biblioteca cv2 para a imagem abrir em uma escala de 0 a 255.
-# pelo matplot a imagem fica em uma escala de 0 a 1
-img_th = th(gray(cv2.imread(img)))
+def fractal(img):
+    log_eta = []
+    log_n_box = []
+    screen_size = np.shape(img)[0]
+    scale = int(np.log(screen_size)/np.log(2))
 
-LogEta = []
-LogNBox = []
-ScreenSize = np.shape(img_th)[0]
-Side = int(np.log(ScreenSize)/np.log(2))
+    for p in range(scale):
+        scale = int(2**p)
+        num_box = 0
+        y = 0
+        num_steps = 0
+        while y <= screen_size:
+            x = 0
+            while x <= screen_size:
+                if np.count_nonzero(img[x:x+scale, y:y+scale]) > 0:
+                    num_box += 1
 
-NumSteps = 0
+                x += scale
+            y += scale
+        num_steps += 1
+        log_eta.append(np.log(scale))
+        log_n_box.append(np.log(1/num_box))
 
-for p in range(Side):
-    Side = int(2**p)
+    coeffs = np.polyfit(log_eta, log_n_box, 1)
 
-    NumBox = 0
-    y = 0
-    while y <= ScreenSize:
-        x = 0
-        while x <= ScreenSize:
-            if np.count_nonzero(img_th[x:x+Side, y:y+Side]) > 0:
-                NumBox += 1
+    return coeffs[0]
 
-            x += Side
-        y += Side
-    NumSteps += 1
-    LogEta.append(np.log(Side))
-    LogNBox.append(np.log(NumBox))
-
-
-coeffs = np.polyfit(LogEta, LogNBox, 1)
-
-plt.plot(LogEta, LogNBox, 'o', mfc='none')
-plt.plot(LogEta, np.polyval(coeffs, LogEta),  label=str(coeffs))
-plt.xlabel('log $\epsilon$')
-plt.ylabel('log N')
-plt.legend()
-plt.show()
+print(fractal(img_th))
