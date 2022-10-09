78c34d93e60f1d2a9b4db047d6a9a82b853e914d
walacevale <wallacevale123@gmail.com>
2022-10-09 18:23:44 -0300

    final

--- box-count.py
+++ box-count.py
@@ -1,43 +1,48 @@
 import matplotlib.pyplot as plt
 import cv2
 import os
-
-from numpy import zeros
+#from scipy.signal import convolve2d
+from tqdm import tqdm
 from Func import *
+
 
 # abrir imagem direto da pasta Dados
 img = os.path.join('Dados', '3.png')
-
 # transforma a imagem em escala de cinza e dps em preto e branco
 # utilizei a biblioteca cv2 para a imagem abrir em uma escala de 0 a 255.
 # pelo matplot a imagem fica em uma escala de 0 a 1
-img_th = th(gray(cv2.imread(img)))/255
-print(np.count_nonzero(img_th[:512,512:]))
-T = 0
+img_th = th(gray(cv2.imread(img)))
 
-width = np.shape(img_th)[1]
-p = np.log(width)/np.log(2)
+LogEta = []
+LogNBox = []
+ScreenSize = np.shape(img_th)[0]
+Side = int(np.log(ScreenSize)/np.log(2))
 
-p = int(np.ceil(p))
-width = 2**p
-n = np.zeros(p+1)
+NumSteps = 0
 
-for g in range(p-1, 0, -1):
-    siz = 2**(p-g)
-    siz2 = int(np.round(siz/2))
-    for i in range(1, (width - siz ), siz):
-        
-        for j in range(1, (width - siz ), siz):
-            #img[i,j] = (img[i,j] or img[i+siz2,j] or img[i,j+siz2] or img[i+siz2,j+siz2] )
-            #img[i,j] = np.sum(img[1: , :])
-            #print(str(siz) + str((width-siz+1)))
-            n[g] = np.sum( img_th[1:(width-siz+1),1:(width-siz+1)])
-    
-r = 2**(np.arange(0,p+1))
-print(r)
-plt.plot(np.log(n),np.log(r), 'o', mfc='none')
+for p in range(Side):
+    Side = int(2**p)
 
-#coeffs = np.polyfit(np.log(n), np.log(r), 1)
-#plt.plot(np.log(n), np.polyval(coeffs,np.log(r)))
-#print(coeffs[0])
+    NumBox = 0
+    y = 0
+    while y <= ScreenSize:
+        x = 0
+        while x <= ScreenSize:
+            if np.count_nonzero(img_th[x:x+Side, y:y+Side]) > 0:
+                NumBox += 1
+
+            x += Side
+        y += Side
+    NumSteps += 1
+    LogEta.append(np.log(Side))
+    LogNBox.append(np.log(NumBox))
+
+
+coeffs = np.polyfit(LogEta, LogNBox, 1)
+
+plt.plot(LogEta, LogNBox, 'o', mfc='none')
+plt.plot(LogEta, np.polyval(coeffs, LogEta),  label=str(coeffs))
+plt.xlabel('log $\epsilon$')
+plt.ylabel('log N')
+plt.legend()
 plt.show()
